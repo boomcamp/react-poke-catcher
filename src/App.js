1d9bb7d944a3d765b971c3c411af4611341626ac
EdwardNayve <dragon.iostream@gmail.com>
2019-06-26 09:49:24 +0800

    submission

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ class App extends React.Component {
               }); 
             } else {
            
-              return customRes; }
+              return customRes; } 
           })
           .then(customRes => {
   
--- src/components/PokemonCaptured.js
+++ src/components/PokemonCaptured.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default function PokemonCaptured(props) {
-	console.log(props.items)
+	console.log(props.items.length)
+	const numCaptured = props.items.length;
 	return (
 		<div className='child'>
-			<h2>Captured</h2>
+			<h2>Captured {numCaptured + '/6'}</h2>
 			<div className='captured'>
 			{props.items.map((poke,index) => (
 				<div key={index} className='capPoke'>
